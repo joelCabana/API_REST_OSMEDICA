4af3df2ff189a4e1eb631a759659dcc6010e4f9f
joelCabana <joelxcabana@gmail.com>
2020-11-05 15:39:14 -0300

    Creacion del Proyecto

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+const { mongoose } = require('./database')
+const cors = require('cors');
+
+// Middlewares
+app.use(express.json({limit: '50mb'}));
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.use('/api/usuario', require('./routes/usuario.router'));
+app.use('/api/afiliado', require('./routes/afiliado.router'));
+app.use('/api/noticia', require('./routes/noticia.router'));
+app.use('/api/pago', require('./routes/pago.routes'));
+app.use('/api/servicio', require('./routes/servicio.router'));
+app.use('/api/novedad', require('./routes/novedad.router'));
+
+// Setting
+app.set('port', process.env.PORT || 3000);
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server started on port`, app.get('port'));
+});
